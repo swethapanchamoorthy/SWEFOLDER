eb71dac99abeb36fd37065a771af26327f612e2c
Swetha.P <pswetha637@gmail.com>
2024-08-21 12:16:55 +0530

    msdhoni

---
+++ python/swetha.py
@@ -0,0 +1,20 @@
+Python 3.7.5 (tags/v3.7.5:5c02a39a0b, Oct 15 2019, 00:11:34) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> print(5)
+5
+>>> print(swetha)
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    print(swetha)
+NameError: name 'swetha' is not defined
+>>> print("swetha")
+swetha
+>>> print(.5)
+0.5
+>>> a=5
+>>> print(a)
+5
+>>> print(type((a)))
+<class 'int'>
+>>> 
